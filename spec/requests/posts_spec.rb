9d78c2d27a225b71c17ce510e18a2bcaef390ebe
rbhatt199924 <rbhatt199924@gmail.com>
2023-04-03 21:20:46 +0530

    add test for request

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "/posts", type: :request do
+
+  describe "GET /index" do
+    before (:example) do
+      get '/users/1/posts'
+    end
+    it "renders a successful response" do
+      expect(response).to be_successful
+    end
+    it "correctly renders the index template" do
+      expect(response).to render_template(:index)
+    end
+    it "contain the placeholder text" do
+      expect(response.body).to include("All the posts by selected user")
+    end
+  end
+
+  describe "GET /show" do
+    before (:example) do
+      get '/users/1/posts/1'
+    end
+    it "renders a successful response" do
+      expect(response).to be_successful
+    end
+    it "correctly renders the show template" do
+      expect(response).to render_template(:show)
+    end
+    it "contain the placeholder text" do
+      expect(response.body).to include("Selected posts by selected user")
+    end
+  end
+end
---
+++ spec/requests/uses_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "/users", type: :request do
+  describe "GET /index" do
+    before (:example) do
+      get '/users'
+    end
+    it "renders a successful response" do
+      expect(response).to be_successful
+    end
+    it "correctly renders the index template" do
+      expect(response).to render_template(:index)
+    end
+    it "contain the placeholder text" do
+      expect(response.body).to include("All the users")
+    end
+  end
+
+  describe "GET /show" do
+    before (:example) do
+      get '/users/1'
+    end
+    it "renders a successful response" do
+      expect(response).to be_successful
+    end
+    it "correctly renders the show template" do
+      expect(response).to render_template(:show)
+    end
+    it "contain the placeholder text" do
+      expect(response.body).to include("Selected the users")
+    end
+  end
+end
