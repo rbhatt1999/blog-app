b72fbc70c6fc5ff5d1b3b65cab56289facf9266b
rbhatt199924 <rbhatt199924@gmail.com>
2023-04-06 16:36:16 +0530

    add comments button and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do 
+      resources :comments, only: [ :new, :create]
+    end
   end
 end
