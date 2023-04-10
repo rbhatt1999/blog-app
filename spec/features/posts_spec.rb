383c1b0340c3d38acc92a43bbab5d7c97018fd7f
rbhatt199924 <rbhatt199924@gmail.com>
2023-04-11 00:09:29 +0530

    fix linter issue

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,20 +1,21 @@
 require 'rails_helper'
-RSpec.describe "Posts", type: :feature do
-  before (:example) do
-    @user = User.create(name: 'John Doe', photo: 'https://e7.pngegg.com/pngimages/636/141/png-clipart-computer-icons-user-s-included-miscellaneous-user-profile.png', bio: 'live to bio')
+RSpec.describe 'Posts', type: :feature do
+  before(:example) do
+    @user = User.create(name: 'John Doe',
+                        photo: 'https://e7.pngegg.com/pngimages/636/141/png-clipart-computer-icons-user-s-included-miscellaneous-user-profile.png', bio: 'live to bio')
     @post = Post.create(title: 'title', text: 'content', author: @user)
     @comment = Comment.create(text: 'comment', post: @post, author: @user)
     @like = Like.create(post: @post, author: @user)
   end
-  describe "GET /posts" do
-    before (:example) do
+  describe 'GET /posts' do
+    before(:example) do
       visit user_posts_path(@user)
     end
-    it "I can see the image" do
+    it 'I can see the image' do
       expect(page.find('img')['src']).to have_content @user.photo
     end
-    it "I can see the username" do
-      expect(page).to have_content("John Doe")
+    it 'I can see the username' do
+      expect(page).to have_content('John Doe')
     end
     it 'I can see the number of posts' do
       expect(page).to have_content('Number of posts: 1')
@@ -40,8 +41,8 @@ RSpec.describe "Posts", type: :feature do
       expect(current_path).to eq user_post_path(@user, @post)
     end
   end
-  describe "GET /posts/:id" do
-    before (:example) do
+  describe 'GET /posts/:id' do
+    before(:example) do
       visit user_post_path(@user, @post)
     end
     it 'I can see the post title' do
@@ -50,13 +51,13 @@ RSpec.describe "Posts", type: :feature do
     it "I can see post author's name" do
       expect(page).to have_content('John Doe')
     end
-    it "I can see how many comments the post has" do
+    it 'I can see how many comments the post has' do
       expect(page).to have_content('Comments: 1')
     end
-    it "I can see how many likes the post has" do
+    it 'I can see how many likes the post has' do
       expect(page).to have_content('Likes: 1')
     end
-    it "I can see the post content" do
+    it 'I can see the post content' do
       expect(page).to have_content('content')
     end
     it "I can see the username of the comment's author" do
