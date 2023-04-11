b97ece982e75590b500f05cfd11819087010f1f2
rbhatt199924 <rbhatt199924@gmail.com>
2023-04-11 19:56:30 +0530

    add migration and routes

--- db/migrate/20230328103546_add_details_to_users.rb
+++ db/migrate/20230328103546_add_details_to_users.rb
@@ -1,7 +1,7 @@
 class AddDetailsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :name, :string
-    add_column :users, :photo, :string
+    add_column :users, :photo, :string, default: "https://img.freepik.com/free-icon/user_318-159711.jpg"
     add_column :users, :bio, :text
     add_column :users, :posts_counter, :integer
   end
