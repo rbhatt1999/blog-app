e644b4e4c1c2565e23e3d93af15bf1acc19d7c2b
Hassaan Baig <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-12 19:16:06 +0500

    Add Destroy method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,24 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
   def index
     @posts = Post.where(author_id: params[:user_id])
     @user = User.find(params[:user_id])
   end
-
   def show
     @post = Post.includes(:author).find(params[:id])
     @user = User.find(params[:user_id])
     @comments = @post.comments
     @likes = @post.likes
   end
-
   def new
     @user = current_user
     @post = Post.new
   end
-
   def create
     @user = current_user
-    @post = @user.posts.new(author: @user, title: params[:post][:title], text: params[:post][:text])
-
+    @post = Post.new(post_params)
+    @post.author = @user
     if @post.save
       @post.update_post_counter
       flash[:notice] = 'Your post was created successfully'
@@ -28,5 +27,22 @@ class PostsController < ApplicationController
       flash.alert = 'sorry, something went wrong!'
       render :new
     end
+  end
+  def destroy
+    @user = User.find(params[:user_id]) # current_user
+    post = Post.find(params[:id])
+    post.comments.destroy_all
+    post.likes.destroy_all
+    if post.destroy
+      flash[:success] = 'Post deleted successfully'
+      redirect_to user_posts_path(@user)
+    else
+      flash.now[:error] = 'Error: Post could not be deleted'
+      redirect_to user_post_path(@user, post)
+    end
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
