e1cf281acd9fecbc15cf129038f1bb2ca28597d6
rbhatt199924 <rbhatt199924@gmail.com>
2023-04-12 19:50:08 +0530

    add comment delete feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   def new
     @comment = Comment.new
     @post = Post.find(params[:post_id])
@@ -7,8 +8,29 @@ class CommentsController < ApplicationController
   def create
     @user = current_user
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(author: @user, post: @post, text: params[:comment][:text])
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    @comment.author = @user
     @comment.save
     redirect_to user_post_path(@user, @post)
+  end
+
+  def destroy
+    @comment = Comment.includes(:author, :post).find(params[:id])
+    user = @comment.author
+    post = @comment.post
+
+    if @comment.destroy
+      flash[:success] = 'Comment deleted successfully'
+    else
+      flash[:error] = 'Error: Comment could not be deleted'
+    end
+    redirect_to user_post_path(user, post)
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
